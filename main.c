7944d68a79fb9327e2515a938d95125d0d5806dc
Ricky Han <hzgdiyer@163.com>
2018-10-04 12:06:16 +0800

    bubble sort

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 //sorting algorithm
+#include <stdio.h>
+
+extern int bubble_sort(int *p, unsigned int n);
 
 int main()
 {
+	int i=0;
+	int d[] = {3,4,1,2,7};
+	bubble_sort(d, sizeof(d)/sizeof(d[0]));	
+
+	for (i=0; i<sizeof(d)/sizeof(d[0]); i++)
+	{
+		printf("%d ", d[i]);
+	}
+
+	printf("\n");
 	return 1;
 }
